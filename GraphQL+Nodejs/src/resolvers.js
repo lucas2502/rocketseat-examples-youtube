231aba8893797a75b99a674ccd2424e08eeb5c0e
lucas2502 <lucas2502@live.com>
2019-07-25 23:46:06 -0300

    Finish test GraphQL

---
+++ GraphQL+Nodejs/src/resolvers.js
@@ -0,0 +1,13 @@
+const users = require('./User');
+
+
+module.exports = {
+    Query: {
+        users: () => User.find(),
+        user: (_, { id }) => User.findById(id)  
+    },
+
+    Mutation: {
+        createUser: (_, { name, email }) => User.create({ name, email}),
+    },
+}; 
